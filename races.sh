fcd6489a2fba3de68ae96ed91bae5d6c0c278b80
Richard Jarvis <richard.a.jarvis@googlemail.com>
2024-11-06 13:41:36 +0000

    added venv activation

--- races.sh
+++ races.sh
@@ -5,6 +5,7 @@ cd /home/pi/Epaper
 . ./epaper.env
 
 # run the display in background
+source /home/pi/.venv/epaper/bin/activate
 python3 ./races.py > races.log 2>&1 &
 # echo the pid & if tty
 
--- races2.sh
+++ races2.sh
@@ -5,6 +5,7 @@ cd /home/pi/Epaper
 . ./epaper.env
 
 # run the display in background
+source /home/pi/.venv/epaper/bin/activate
 python3 ./races2.py > races2.log 2>&1 &
 # echo the pid & if tty
 
